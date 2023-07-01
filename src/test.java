565edb7d925c15569b532dfae92a9d401deb0154
jf <2361400512@qq.com>
2023-07-01 21:18:21 +0800

    111

---
+++ src/test.java
@@ -0,0 +1,12 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * @Description :
+ * @Author: JiFei
+ * @Date :2023/7/1
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
