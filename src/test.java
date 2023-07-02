688726ec2523ccefba38f84ea601028d325fbc55
jf <2361400512@qq.com>
2023-07-02 12:22:33 +0800

    444

--- src/test.java
+++ src/test.java
@@ -9,5 +9,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class test {
     public static void main(String[] args) {
         System.out.println("4444");
+        System.out.println("555");
     }
 }
