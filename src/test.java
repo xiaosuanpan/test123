38a836b5a8654c47a0d14d6f67811ccd9a7cff43
jf <2361400512@qq.com>
2023-07-02 12:20:11 +0800

    Revert "111"
    This reverts commit 565edb7d

--- src/test.java
+++ src/test.java
@@ -1,14 +1,1 @@
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
-/**
- * @Description :
- * @Author: JiFei
- * @Date :2023/7/1
- */
-public class test {
-    public static void main(String[] args) {
-        System.out.println("111");
-        System.out.println("222");
-        System.out.println("333");
-    }
-}
