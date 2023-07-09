76716209fc05e4ee1e4f53f229c45ea46096eaaf
jf <2361400512@qq.com>
2023-07-09 09:47:17 +0800

    2222222222

--- src/test.java
+++ src/test.java
@@ -8,13 +8,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
  */
 public class test {
     public static void main(String[] args) {
-        System.out.println("111");
-        System.out.println("2222");
-        System.out.println("333");
-        System.out.println("4444");
-        System.out.println("555");
-        System.out.println("666");
-        System.out.println("777");
-
+        System.out.println("222222");
     }
 }
